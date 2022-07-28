19ce444c6060fad582b61c394a625c529271e3f3
dcebeci <cebecidogukan@gmail.com>
2022-07-28 23:54:57 +0300

    file & folder structure yapıldı.

---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,7 @@
+export {default as About} from './container/About';
+export {default as Footer} from './container/Footer';
+export {default as Header} from './container/Header';
+export {default as Project} from './container/Project';
+export {default as Skills} from './container/Skills';
+export {default as Testimonial} from './container/Testimonial';
+export {default as Contact} from './container/Contact';
