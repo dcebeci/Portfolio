9452e312cf0bcfdb9068aa24dc4746fe8bf8a788
dcebeci <cebecidogukan@gmail.com>
2022-08-03 22:50:22 +0300

    .

--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,16 +1,2 @@
 export {default as  Navbar} from './Navbar';
 
-<form class="form-inline" action="/action_page.php">
-<div class="form-group">
-  <label for="email">Email address:</label>
-  <input type="email" class="form-control" id="email">
-</div>
-<div class="form-group">
-  <label for="pwd">Password:</label>
-  <input type="password" class="form-control" id="pwd">
-</div>
-<div class="checkbox">
-  <label><input type="checkbox"> Remember me</label>
-</div>
-<button type="submit" class="btn btn-default">Submit</button>
-</form> 
