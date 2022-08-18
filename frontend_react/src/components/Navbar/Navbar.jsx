d12035a01ab11e98efe58c41292e4bef347d69f6
dcebeci <cebecidogukan@gmail.com>
2022-08-18 09:07:35 +0300

    navbar export düzenlendi. Scss hatası giderildi.

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
-import React, {useState} from 'react';
-import {images} from '../../constants';
-import {HiMenuAlt4, HiX} from 'react-icons/hi';
-import {motion} from 'framer-motion';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 import './Navbar.scss';
 
-const Navbar =() => {
+const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+
   return (
-    <nav>
-      <div >
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
-        </div>
-        <ul className="app__navbar-links">
+      </div>
+      <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
@@ -19,6 +21,7 @@ const Navbar =() => {
           </li>
         ))}
       </ul>
+
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,2 +1,9 @@
-export {default as  Navbar} from './Navbar';
+import NavigationDots from './NavigationDots';
+import SocialMedia from './SocialMedia';
+import Navbar from './Navbar/Navbar';
 
+export {
+  NavigationDots,
+  SocialMedia,
+  Navbar,
+};
