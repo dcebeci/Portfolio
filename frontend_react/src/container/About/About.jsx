e6e864ca2cc0e1db55cfb6b9893ab9e6cd106a7a
dcebeci <cebecidogukan@gmail.com>
2022-10-10 23:57:20 +0300

    api key girilecek.

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -5,8 +5,8 @@ import {images} from '../../constants';
 
 const abouts = [{
   title: 'Web development', description: 'I can build web applications using React & .net MVC', imgUrl: images.about01},
-  {title: 'Frontend Development', description: 'I am good at web desing',imgUrl: images.about02},
-  {title: 'Backend Development', description: 'I know .net MVC and EFC',imgUrl: images.about04
+  {title: 'Frontend Development', description: 'I know react an js on front side ',imgUrl: images.about02},
+  {title: 'Backend Development', description: 'I know .net MVC and EFC',imgUrl: images.about03
 },];
 
 
