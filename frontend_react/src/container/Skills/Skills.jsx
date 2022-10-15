4c33ac9fcfe096c7651c49d31a3eaf370167179d
dcebeci <cebecidogukan@gmail.com>
2022-10-15 23:40:29 +0300

    reacttooltip eklendi.

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -69,7 +69,14 @@ const Skills = () => {
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
                     </motion.div>
-                  
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
                   </>
                 ))}
               </motion.div>
