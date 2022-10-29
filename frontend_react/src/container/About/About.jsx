bbb3cf719eec1cf504c95d113e6b012ebe1ee712
dcebeci <cebecidogukan@gmail.com>
2022-10-29 20:42:11 +0300

    webpckden hata gösteriyor sanityle bağlantı hatası

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -17,13 +17,6 @@ const About=() =>{
  
   return (
     <>
-    <h2 className='head-text'>
-    I know that
-    <span>Good Desing</span>
-    <br/>
-    means
-    <span>Good Business</span>
-    </h2>
 
     <div className='app__profiles'>
     {abouts.map((about, index) => (
