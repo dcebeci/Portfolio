d43efdb0ab4554c53bd0b33bb64300146915521c
dcebeci <cebecidogukan@gmail.com>
2022-10-29 20:38:42 +0300

    clientden webpackden hata olabilir ya env çekmiyor yada sanity

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,10 +1,11 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
+
 export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
-    apiVersion: '22-02-01',
+    apiVersion: '2022-10-29',
     userCdn: true,
     token: process.env.REACT_APP_SANITY_TOKEN,
 });
